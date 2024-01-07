04ecb9fd342a20fc37f5a3023f7941584cea47c5
sarika-tops <130426640+sarika-tops@users.noreply.github.com>
2024-01-07 15:20:02 +0530

    Update ArmstrongNumber.java

--- Core/ArmstrongNumber.java
+++ Core/ArmstrongNumber.java
@@ -6,24 +6,23 @@ import java.util.Scanner;
 
 public class ArmstrongNumber {
        public static void main(String[] args) {
-   	   int i, arms=0, remin, j;
-   	   System.out.println("use only 3 digits");
-   	   Scanner sc = new Scanner(System.in);
-   	   i = sc.nextInt();
-   	   j = i;
-   	   while (i>0) {
-   	    	remin = i%10;
-   	    	arms = (remin*remin*remin)+arms;
-   	    	i = i/10;
-   	    }
-   	   if (j==arms) { 
-   	        System.out.println("Number is armstrong"); 
-   	    	}
-   	   else {
-   	    	System.out.println("Number is not armstrong");
-   	    }
-   	   while (arms<=99) {   
-   	    	System.out.println("");
+   	   	Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int n = sc.nextInt();
+		int arm = 0, digit, num ;
+		num = n;
+		while (num!=0) {
+			digit = num % 10;
+			arm += Math.pow(digit, 3);
+			num /= 10;
+		}
+		if (arm==n) {
+			System.out.println(+n+ " is an armstrong number");
+		}
+		else {
+			System.out.println(+n+ " is not an armstrong number");
+		}
+
    	    }
 	}
 }
